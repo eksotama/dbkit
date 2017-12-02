d43db39de94f1eb7c1f47e6f8027068b73b45d1f
tatwd <leoking9641@hotmail.com>
2017-12-02 18:12:09 +0800

    :bug: Remodified upstream

--- DbKit.cs
+++ DbKit.cs
@@ -38,7 +38,7 @@ namespace DbKit
         //       }
         //   }
         //
-        private static readonly string ConnectorClassName = typeof(MsSqlConnector);
+        private static readonly string ConnectorClassName = typeof(MsSqlConnector).ToString();
 
         // Summary:
         //   获取当前程序集全名，只读
